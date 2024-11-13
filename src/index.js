87516819ba0cdf36f17e8323f39fd448c2cc335f
25vudinhvinh <25vudinhvinh@gmail.com>
2024-11-13 19:54:21 +0700

    sass

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 
+// middleware file tinh
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
